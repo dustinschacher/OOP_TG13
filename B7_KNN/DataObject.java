a5b9d705c61aca14876ca14595b011b419ed55ae
dustinschacher <123462773+dustinschacher@users.noreply.github.com>
2024-12-17 11:54:16 +0000

    17.12

---
+++ B7_KNN/DataObject.java
@@ -0,0 +1,12 @@
+public class DataObject {
+
+    public DataObject(int i, int j, int k) {
+        //TODO Auto-generated constructor stub
+    }
+    public DataObject(int i, int j) {
+        //TODO Auto-generated constructor stub
+    }
+    int x;
+    int y;
+    int klasse;
+}
