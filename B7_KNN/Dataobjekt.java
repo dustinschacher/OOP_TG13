ce3f1af7701d8a416231bd3743437f5626d0c44c
dustinschacher <123462773+dustinschacher@users.noreply.github.com>
2025-01-08 10:07:37 +0000

    gfdhgf

---
+++ B7_KNN/Dataobjekt.java
@@ -0,0 +1,31 @@
+public class Dataobjekt {
+
+    int x;
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    int y;
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    int klasse;
+    public int getKlasse() {
+        return klasse;
+    }
+    public void setKlasse(int klasse) {
+        this.klasse = klasse;
+    }
+    public Dataobjekt(int x, int y, int klasse) {
+        this.x = x;
+        this.y = y;
+        this.klasse = klasse;
+    }
+
+}
+
